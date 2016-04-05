c2cbef6442670c11d322ad24e1642e8e66c34dba
HyeonJu Jeong <0106grace@naver.com>
2016-04-05 20:53:35 +0900

    #4 send user input by using

--- app/src/main/java/kr/hs/emirim/grace0106/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/grace0106/monchicken/MainActivity.java
@@ -46,6 +46,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         try{
             Toast.makeText(this, name + "배고파요", Toast.LENGTH_LONG).show();
             Intent intent = new Intent(this, ResultActivity.class);
+            intent.putExtra("name", name);
+            intent.putExtra("age", 10);
             startActivity(intent);
         }catch(NullPointerException e) {
             Toast.makeText(this, "이름을 입력해 주세요!", Toast.LENGTH_LONG).show();
--- app/src/main/java/kr/hs/emirim/grace0106/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/grace0106/monchicken/ResultActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.grace0106.monchicken;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
@@ -19,5 +20,10 @@ public class ResultActivity extends ActionBarActivity {
 
         mResult = (TextView)findViewById(R.id.result);
         mResult.setText("결과");
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        int age = intent.getIntExtra("age", -1);
+        mResult.setText(name + "님, 안녕하세요!");
     }
 }
