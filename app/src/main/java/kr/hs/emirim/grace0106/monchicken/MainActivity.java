f1b796f02d8213ef445541d8d9596e11dd018289
HyeonJu Jeong <0106grace@naver.com>
2016-04-05 20:32:20 +0900

    #4 handle user input(name)

--- app/src/main/java/kr/hs/emirim/grace0106/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/grace0106/monchicken/MainActivity.java
@@ -27,13 +27,31 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
-        mName.setText("");
+        mName.setText(null);
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "배고파요", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, ResultActivity.class);
-        startActivity(intent);
+
+        String name = mName.getText().toString();
+//        if (name==null)
+//        {
+//            Toast.makeText(this, "이름을 입력해 주세요!", Toast.LENGTH_LONG).show();
+//        }else{
+//            Toast.makeText(this, name + "배고파요", Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(this, ResultActivity.class);
+//            startActivity(intent);
+//        }
+
+        try{
+            Toast.makeText(this, name + "배고파요", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, ResultActivity.class);
+            startActivity(intent);
+        }catch(NullPointerException e) {
+            Toast.makeText(this, "이름을 입력해 주세요!", Toast.LENGTH_LONG).show();
+        }catch(Exception e){
+            Toast.makeText(this, "뭔지 모르지만 잘 안되네요!", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
