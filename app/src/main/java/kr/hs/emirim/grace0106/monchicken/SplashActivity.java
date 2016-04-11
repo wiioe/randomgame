5ec86e3eac8bdccf73edc5d9f8a085905c7dd2ca
HyeonJu Jeong <0106grace@naver.com>
2016-04-11 21:01:36 +0900

    #8 Splash Activity

---
+++ app/src/main/java/kr/hs/emirim/grace0106/monchicken/SplashActivity.java
@@ -0,0 +1,31 @@
+package kr.hs.emirim.grace0106.monchicken;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
